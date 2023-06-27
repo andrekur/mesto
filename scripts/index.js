168719b6e2da0ae0a0dbd4b246c288fce2087c3a
andre <kurepin.andre@yandex.ru>
2023-06-27 23:46:21 +0300

    Перед первой проверкой

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,37 @@
-const menuItemAbout = document.querySelector('.profile__edit-button')
-const popupAbout = document.querySelector('.popup_edit_user_info')
-const buttonCloseAboutPopup = popupAbout.querySelector('.popup__close')
+let buttonProfileEdit= document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let buttonClosePopup = popup.querySelector('.popup__close');
+let userFullName = document.querySelector('.profile__full-name');
+let userJob = document.querySelector('.profile__description');
 
-function togglePopup(ev) {
-  popupAbout.classList.toggle('popup_opened')
+let formElement = document.querySelector('.popup__save');
+let nameInput = popup.querySelector('.popup__input-text_name');
+let jobInput = popup.querySelector('.popup__input-text_job');
+
+function openPopup() {
+  if (!popup.classList.contains('popup_opened')) {
+    console.log(nameInput)
+    nameInput.value = userFullName.innerText;
+    jobInput.value = userJob.innerText;
+    popup.classList.add('popup_opened')
+  }
 }
 
-menuItemAbout.addEventListener('click', togglePopup)
-buttonCloseAboutPopup.addEventListener('click', togglePopup)
+function closePopup() {
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened')
+  }
+}
+
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    userFullName.innerText = nameInput.value;
+    userJob.innerText = jobInput.value;
+    popup.classList.remove('popup_opened')
+    closePopup()
+}
+
+formElement.addEventListener('click', handleFormSubmit);
+buttonProfileEdit.addEventListener('click', openPopup)
+buttonClosePopup.addEventListener('click', closePopup)
